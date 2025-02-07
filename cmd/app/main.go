bc63c4183985957e3350fd1756bba4447295d3ea
TyroneHe-0926 <tyrone99926@gmail.com>
2025-02-07 00:29:53 -0500

    chore: init schema sturctures for users:

---
+++ cmd/app/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"cs348-project-group1/pkg/schema"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+
+	_ "github.com/marcboeker/go-duckdb"
+)
+
+func main() {
+	schema.CreateUsersTable()
+
+	db, err := sql.Open("duckdb", "./moviedb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`INSERT INTO users SELECT * FROM read_csv('./users.csv')`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	user_table := schema.User{}
+
+	row := db.QueryRow(`SELECT * FROM users`)
+	err = row.Scan(
+		&user_table.Uid, &user_table.Name,
+		&user_table.Dob, &user_table.Password,
+		&user_table.Language,
+	)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		log.Println("no rows")
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("uid: %d, name: %s, dob: %s, password: %s, language: %s\n", user_table.Uid, user_table.Name, user_table.Dob, user_table.Password, user_table.Language)
+}
---
+++ pkg/schema/user.go
@@ -0,0 +1,37 @@
+package schema
+
+import (
+	"database/sql"
+	"log"
+)
+
+var users_schema = `
+CREATE TABLE users (
+	uid INT NOT NULL PRIMARY KEY,
+    name VARCHAR(255),
+    dob DATE,
+    password VARCHAR(255),
+	language VARCHAR(255)
+)`
+
+type User struct {
+	Uid      int    `db:"uid"`
+	Name     string `db:"name"`
+	Dob      string `db:"dob"`
+	Password string `db:"password"`
+	Language string `db:"language"`
+}
+
+func CreateUsersTable() {
+	db, err := sql.Open("duckdb", "./moviedb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(users_schema)
+	if err != nil {
+		log.Fatal(err)
+	}
+	print("User Table Created")
+}
