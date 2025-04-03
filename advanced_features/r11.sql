9c14b7d7610102bbc3b638e3a562b2a295e277c3
Justin Jonany <jonanyj@gmail.com>
2025-04-02 21:11:36 -0400

    r11.sql

--- advanced_features/r11.sql
+++ advanced_features/r11.sql
@@ -1,5 +1,4 @@
-WITH RECURSIVE 
-temp AS (
+WITH RECURSIVE temp AS (
     SELECT pID FROM people WHERE name = 'Fred Astaire' LIMIT 1 -- !!! PUT ACTOR NAME HERE !!!
 ),
 ActorNetwork (anc, des, depth) AS (
@@ -12,10 +11,17 @@ ActorNetwork (anc, des, depth) AS (
     SELECT AN.des AS anc, W2.pID AS des, depth + 1
     FROM ActorNetwork AN, workedOn W1, workedOn W2
     WHERE AN.des = W1.pID AND W1.tID = W2.tID AND W1.pID != W2.pID
-    AND AN.depth < 2 -- !!! PUT NUMBER OF CONNECTIONS HERE !!!
-) 
-
-SELECT P1.name AS anc, P2.name AS des 
-FROM (SELECT DISTINCT anc, des FROM ActorNetwork) AS AN
-JOIN people P1 ON AN.anc = P1.pID 
-JOIN people P2 ON AN.des = P2.pID;
+    AND AN.depth < 3 -- !!! PUT NUMBER OF CONNECTIONS HERE !!!
+), DistinctPairs AS (
+-- Create an ordered pair for each connection so that pair (A,B) is the same as (B,A)
+SELECT CASE WHEN P1.name < P2.name THEN P1.name ELSE P2.name END AS actor1,
+    CASE WHEN P1.name < P2.name THEN P2.name ELSE P1.name END AS actor2,
+    AN.depth
+FROM (SELECT DISTINCT anc, des, depth FROM ActorNetwork) AS AN
+    JOIN people P1 ON AN.anc = P1.pID
+    JOIN people P2 ON AN.des = P2.pID
+)
+SELECT actor1, actor2, MIN(depth) AS depth  
+FROM DistinctPairs
+GROUP BY actor1, actor2
+ORDER BY depth;
